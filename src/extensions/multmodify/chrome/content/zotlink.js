dbdc6b4e7835e4d04bb71d8baed98286a417b1a3
serializedowen <wjh199455@gmail.com>
2015-10-26 13:48:17 -0400

    Sketch for multiselected-modification

---
+++ src/extensions/multmodify/chrome/content/zotlink.js
@@ -0,0 +1,21 @@
+Zotero.ZotLink = {
+
+    init: function() {
+        var observerID = Zotero.Notifier.registerObserver(this.observer, ["item"]);
+        window.addEventListener("unload", function() {
+            Zotero.Notifier.unregisterObserver(observerID);
+        });
+    },
+
+    observer: {
+        notify: function(event, type, ids, extra) {
+            if (event === "add") {
+                var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                   .getService(Components.interfaces.nsIPromptService);
+                ps.confirm(window, "Duplication Check", "Do you want to check possible duplications of this item in other libraries?");
+            }
+        },
+    },
+};
+
+window.addEventListener("load", function() { Zotero.ZotLink.init(); });
