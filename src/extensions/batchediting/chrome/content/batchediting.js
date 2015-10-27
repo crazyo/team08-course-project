de870ba5463073ac2ccf2ea76a4df436f07ae8db
serializedowen <wjh199455@gmail.com>
2015-10-27 10:26:29 -0400

    Daily Commit

--- src/extensions/batchediting/chrome/content/batchediting.js
+++ src/extensions/batchediting/chrome/content/batchediting.js
@@ -2,20 +2,40 @@ Zotero.BatchEditing = {
 
     init: function() {
         var observerID = Zotero.Notifier.registerObserver(this.observer, ["item"]);
+        var itempane = document.getElementById('otero-item-pane-content');
+        var ZoteroPane = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser").ZoteroPane;
+
+        Object.observe(ZoteroPane.itemsView.selection.count, function(){
+          ps.confirm(window, "Duplication Check", "abc");
+        })
         window.addEventListener("unload", function() {
             Zotero.Notifier.unregisterObserver(observerID);
         });
-},
+    },
 
-observer: {
-  notify: function(event, type, ids, extra) {
-    if (event === "add") {
-      var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-        .getService(Components.interfaces.nsIPromptService);
-      ps.confirm(window, "Duplication Check", "Do you want to check possible duplications of this item in other libraries?");
-    }
-  },
-},
+    observer: {
+        notify: function(event, type, ids, extra) {
+
+
+
+            if (ZoteroPane.itemsView.selection.count > 1){
+                var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                   .getService(Components.interfaces.nsIPromptService);
+                ps.confirm(window, "Duplication Check", a[0]);
+            }
+
+
+            if (event === "add") {
+
+
+
+                var a = ZoteroPane.getSelectedItems();
+                var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                   .getService(Components.interfaces.nsIPromptService);
+                ps.confirm(window, "Duplication Check", ZoteroPane.itemsView.selection.count);
+            }
+        },
+    },
 };
 
-window.addEventListener("load", function() { Zotero.ZotLink.init(); });
+window.addEventListener("load", function() { Zotero.BatchEditing.init(); });
