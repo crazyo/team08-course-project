d300741cf57cead4bb80d0510f32f4c72a810d26
Xufeng Wang <xufeng.yo.wang@gmail.com>
2015-11-05 00:26:04 -0500

    finished linking for 1 level; deeper levels may need graph data structure

--- src/extensions/zotlink/chrome/content/zotlink.js
+++ src/extensions/zotlink/chrome/content/zotlink.js
@@ -1,8 +1,10 @@
 Zotero.ZotLink = {
     DB: null,
-
     // cache database invoking result to reduce database-access overhead
     links: [],
+
+    // current observerID
+    observerID: null,
 
 
     init: function() {
@@ -19,23 +21,55 @@ Zotero.ZotLink = {
             this.links.push([Number(rows[i].item1id), Number(rows[i].item2id)]);
         }
 
-        var observerID = Zotero.Notifier.registerObserver(this.observer, ["item"]);
+        this.observerID = Zotero.Notifier.registerObserver(this.observer, ["item"]);
 
         window.addEventListener("unload", function() {
-            Zotero.Notifier.unregisterObserver(observerID);
+            Zotero.Notifier.unregisterObserver(this.observerID);
         });
     },
 
     observer: {
         notify: function(event, type, ids, extra) {
+            // keep a reference to our ZotLink object
+            var zotlink = Zotero.ZotLink;
+
             var sql;
             if (event === "delete") {
                 sql = "DELETE FROM links WHERE item1id IN (" + ids + ") OR item2id IN (" + ids + ");";
-                this.DB.query(sql);
+                zotlink.DB.query(sql);
                 // also update the cache to reduce database access
-                for (var i = this.links.length - 1; i >= 0; i--) {
-                    if (ids.indexOf(this.links[i][0]) !== -1 || ids.indexOf(this.links[i][1] !== -1)) {
-                        this.links.splice(i, 1);
+                for (i = zotlink.links.length - 1; i >= 0; i--) {
+                    if (ids.indexOf(zotlink.links[i][0]) !== -1 || ids.indexOf(zotlink.links[i][1] !== -1)) {
+                        zotlink.links.splice(i, 1);
+                    }
+                }
+            }
+            else if (event === "modify") {
+                // loop over changed items
+                for (var i = 0; i < ids.length; i++) {
+                    // get the changed item
+                    var id = ids[i];
+                    var source = Zotero.Items.get(id);
+
+                    // change all items that are linked to this item
+                    var target;
+                    for (var j = 0; j < zotlink.links.length; j++) {
+                        if (zotlink.links[j][0] !== id && zotlink.links[j][1] !== id) {
+                            continue;
+                        }
+
+                        target = zotlink.links[j][0] === id ?
+                                 Zotero.Items.get(zotlink.links[j][1]) :
+                                 Zotero.Items.get(zotlink.links[j][0]);
+
+                        // temporarily stop listening to events
+                        Zotero.Notifier.unregisterObserver(zotlink.observerID);
+
+                        source.clone(false, target);
+                        target.save();
+
+                        // resume listening to events
+                        zotlink.observerID = Zotero.Notifier.registerObserver(zotlink.observer, ["item"]);
                     }
                 }
             }
@@ -50,11 +84,40 @@ Zotero.ZotLink = {
                           selectedItems);
     },
 
-    createLink: function(oldID, newID) {
-        var sql = "INSERT INTO links VALUES (" + oldID + ", " + newID + ");";
+    createLinkedCopy: function(source, targetLibraryID, targetCollectionID) {
+        // temporarily stop listening to events
+        Zotero.Notifier.unregisterObserver(this.observerID);
+
+        // 1. make a copy at the target location
+        var newItem, newItemID;
+
+        newItem = new Zotero.Item(source.itemTypeID);
+        // add the item to the target library
+        newItem.libraryID = targetLibraryID || null;
+        newItemID = newItem.save();
+        newItem = Zotero.Items.get(newItemID);
+        // copy over all the information
+        source.clone(false, newItem);
+        newItem.save();
+        // add the item to the target collection
+        if (targetCollectionID) {
+            Zotero.Collections.get(targetCollectionID).addItem(newItemID);
+        }
+
+        // 2. link them
+        var sql = "INSERT INTO links VALUES (" + source.id + ", " + newItemID + ");";
         this.DB.query(sql);
         // also update the cache to reduce database access
-        this.links.push([oldID, newID]);
+        this.links.push([source.id, newItemID]);
+
+        // resume listening to events
+        this.observerID = Zotero.Notifier.registerObserver(this.observer, ["item"]);
+    },
+
+    createLinkedCopies: function(sources, targetLibraryID, targetCollectionID) {
+        for (var i = 0; i < sources.length; i++) {
+            this.createLinkedCopy(sources[i], targetLibraryID, targetCollectionID);
+        }
     },
 };
 
