91c4c25ebdbfe71e13c0f4dad80aa199ff522257
Xufeng Wang <xufeng.yo.wang@gmail.com>
2015-11-10 14:11:42 -0500

    minor change

--- src/extensions/batchediting/chrome/content/batchediting.js
+++ src/extensions/batchediting/chrome/content/batchediting.js
@@ -1,16 +1,8 @@
 Zotero.BatchEditing = {
 
     init: function() {
-        var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                    .getService(Components.interfaces.nsIPromptService);
         var tagmenu = document.getElementById("view-settings-popup");
         var batchedit = document.createElement("menuitem");
-        var tags = Zotero.Tags.getAll();
-        for (var property in tags) {
-            promptService.alert(null, "number", property);
-            promptService.alert(null, "dsfds", tags[property].name);
-            promptService.alert(null, "dsfds", tags[property].type);
-        }
         batchedit.setAttribute("label", "batch edit tags");
         batchedit.setAttribute("oncommand", "Zotero.BatchEditing.openDialog();");
         tagmenu.appendChild(batchedit);
