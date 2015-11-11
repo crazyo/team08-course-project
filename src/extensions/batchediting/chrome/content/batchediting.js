5627517b47c2689327c05ccfb3c18295ada40d79
Xufeng Wang <xufeng.yo.wang@gmail.com>
2015-11-10 21:38:46 -0500

    Revert "Resolved Conflicts"
    This reverts commit 8cd4ab80c9fbc9b94db81f30bccb13d79e14e8f7.

--- src/extensions/batchediting/chrome/content/batchediting.js
+++ src/extensions/batchediting/chrome/content/batchediting.js
@@ -1,5 +1,4 @@
 Zotero.BatchEditing = {
-            
 
     init: function() {
         var tagmenu = document.getElementById("view-settings-popup");
@@ -19,30 +18,6 @@ Zotero.BatchEditing = {
         window.openDialog("chrome://batchediting/content/batchtags.xul",
                           "",
                           "chrome,centerscreen,modal,resizable=no");
-
-
-    edittags: function(){
-        var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
-        var selectedItems = ZoteroPane.getSelectedItems();
-
-        if (ZoteroPane.itemsView.selection.count > 1){
-            window.openDialog("chrome://batchediting/content/batchtags.xul",
-                              "",
-                              "chrome,centerscreen,modal,resizable=yes",
-                              selectedItems);
-        } else{
-            ps.confirm(window, "Warning", "You have to select more items to use batch edit.");
-        }
-    },
-
-    observer: {
-        notify: function(event, type, ids, extra) {
-
-            if (event === "add" || event == 'modify' || event == 'delete') {
-                var a = ZoteroPane.getSelectedItems();
-                ps.confirm(window, "Duplication Check", ZoteroPane.itemsView.selection.count);
-            }
-        },
     },
 };
 
