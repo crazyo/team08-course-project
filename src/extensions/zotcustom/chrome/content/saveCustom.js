6aeeb68caa2be217a6bbea1a38fdce074b3e9bb0
raunM <raun.mathur@gmail.com>
2015-11-24 16:58:35 -0500

    fixed bug for not saving as .csl properly

--- src/extensions/zotcustom/chrome/content/saveCustom.js
+++ src/extensions/zotcustom/chrome/content/saveCustom.js
@@ -3,17 +3,17 @@ Components.utils.import("resource://gre/modules/FileUtils.jsm");
 
 var SaveCustom = new function() {
 
-	var format = opener.document.getElementById("new-citation-format");
+	var format = document.getElementById("new-citation-format");
 
     //save custom citation to csl file locally
 	this.save = function() {
-        var saveName = document.getElementById("save-citation-textbox");
         //using csledit from zotero source code for filepicker part
         //create file and save to folder user selects
         var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(Components.interfaces.nsIFilePicker);
         fp.init(window, "Save As", Components.interfaces.nsIFilePicker.modeSave);
         //add csl extension
         fp.appendFilter("Citation Style Language", "*.csl");
+        fp.defaultString = ".csl";
         var ret = fp.show();
         if (ret == fp.returnOK || ret == fp.returnReplace) {
             var fos = Components.classes["@mozilla.org/network/file-output-stream;1"].
@@ -23,7 +23,7 @@ var SaveCustom = new function() {
             var os = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
             createInstance(Components.interfaces.nsIConverterOutputStream);
             os.init(fos, "UTF-8", 0, 0x0000);
-            os.writeString(this.parseCitation(format.value, saveName.value));
+            os.writeString(this.parseCitation(format.value, "customCSL"));
             os.close();
             fos.close();
         }
@@ -39,7 +39,7 @@ var SaveCustom = new function() {
         //required fields for csl file
         finalCSL = '<?xml version="1.0" encoding="utf-8"?>\n<style xmlns="http://purl.org/net/xbiblio/csl" ' +
             'class="in-text" version="1.0" demote-non-dropping-particle="never">\n\t<info>\n\t\t<title>' + saveName +
-            '</title>\n\t\t<id>http://www.zotero.org/styles/' + saveName +
+            '</title>\n\t\t<id>http://www.zotero.org/styles/'  + saveName +
             '</id>\n\t\t<updated>2015-04-07T11:18:00+00:00</updated>\n\t</info>\n\t<bibliography et-al-min="6" ' +
             'et-al-use-first="1" second-field-align="flush" entry-spacing="0" line-spacing="2">\n\t' +
             '<layout>';
