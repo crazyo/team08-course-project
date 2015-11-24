d6a3ee0a227abad0755df4b5daf8788e8187a983
raunM <raun.mathur@gmail.com>
2015-11-24 15:56:04 -0500

    Can now save new csl to users selected location

--- src/extensions/zotcustom/chrome/content/saveCustom.js
+++ src/extensions/zotcustom/chrome/content/saveCustom.js
@@ -7,31 +7,24 @@ var SaveCustom = new function() {
 	var format = opener.document.getElementById("new-citation-format");
 	this.save = function() {
         var saveName = document.getElementById("save-citation-textbox");
-        //using https://developer.mozilla.org/en-US/Add-ons/Code_snippets/File_I_O#Creating_a_file_object
-        //create file and save to style folder
-        var file = Components.classes["@mozilla.org/file/local;1"].
-        createInstance(Components.interfaces.nsILocalFile);
-
-        //wip: can save anywhere.
-        file.initWithPath("C:\\Users\\Raunak\\Desktop\\zotero proj\\team08-course-project\\src\\modules\\zotero\\styles");
-        file.append(saveName.value + ".csl");
-
-        //write to file
-        var ostream = FileUtils.openSafeFileOutputStream(file);
-        var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
-        createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
-        converter.charset = "UTF-8";
-        var istream = converter.convertToInputStream(this.parseCitation(format.value, saveName.value));
-
-        // The last argument (the callback) is optional.
-        NetUtil.asyncCopy(istream, ostream, function (status) {
-            if (!Components.isSuccessCode(status)) {
-                window.alert("Did not write anything.");
-                return;
-            }
-
-            // Data has been written to the file.
-        });
+        //using csledit from zotero source code for filepicker part
+        //create file and save to folder user selects
+        var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(Components.interfaces.nsIFilePicker);
+        fp.init(window, "Save As", Components.interfaces.nsIFilePicker.modeSave);
+        fp.appendFilter("Citation Style Language", "*.csl");
+        var ret = fp.show();
+        if (ret == fp.returnOK || ret == fp.returnReplace) {
+            var fos = Components.classes["@mozilla.org/network/file-output-stream;1"].
+            createInstance(Components.interfaces.nsIFileOutputStream);
+            // write, create, truncate
+            fos.init(fp.file, 0x02 | 0x08 | 0x20, 0664, 0);
+            var os = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
+            createInstance(Components.interfaces.nsIConverterOutputStream);
+            os.init(fos, "UTF-8", 0, 0x0000);
+            os.writeString(this.parseCitation(format.value, saveName.value));
+            os.close();
+            fos.close();
+        }
         window.close();
     }
 
@@ -42,13 +35,12 @@ var SaveCustom = new function() {
         var removeBracersFormat = customFormat.split('{').join('');
         var finalFormat = removeBracersFormat.split('}');
         //required fields for csl file
-        var requiredFields = '<?xml version="1.0" encoding="utf-8"?>\n<style xmlns="http://purl.org/net/xbiblio/csl" ' +
+        finalCSL = '<?xml version="1.0" encoding="utf-8"?>\n<style xmlns="http://purl.org/net/xbiblio/csl" ' +
             'class="in-text" version="1.0" demote-non-dropping-particle="never">\n\t<info>\n\t\t<title>' + saveName +
             '</title>\n\t\t<id>http://www.zotero.org/styles/' + saveName +
             '</id>\n\t\t<updated>2015-04-07T11:18:00+00:00</updated>\n\t</info>\n\t<bibliography et-al-min="6" ' +
             'et-al-use-first="1" second-field-align="flush" entry-spacing="0" line-spacing="2">\n\t' +
             '<layout>';
-        finalCSL = requiredFields;
         //go through format fields and create csl using output
         var i;
         for(i = 0; i < finalFormat.length-1; i++){
