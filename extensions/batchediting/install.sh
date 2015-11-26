e2ac0c0029ed67ac92e16b6b341c12112fcf7e15
serializedowen <wjh199455@gmail.com>
2015-11-26 18:43:12 -0500

    Modified building script
    Modified building script to copy over to parent folder

--- extensions/batchediting/install.sh
+++ extensions/batchediting/install.sh
@@ -1,2 +1,4 @@
-rm arch.xpi
-zip -r arch.xpi chrome chrome.manifest install.rdf
+rm zotbatch.xpi
+rm ../zotbatch.xpi
+zip -r zotbatch.xpi chrome chrome.manifest install.rdf
+cp zotbatch.xpi ../zotbatch.xpi
